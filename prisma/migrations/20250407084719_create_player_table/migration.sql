a4d88cec023f6e9fc9ee9b97b7f0e5f386bbba02
LinCie <aldiofernanda@protonmail.com>
2025-04-07 15:47:26 +0700

    feat: create player table

---
+++ prisma/migrations/20250407084719_create_player_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Player` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `type` VARCHAR(191) NULL,
+    `typeId` INTEGER NULL,
+    `size` VARCHAR(191) NULL,
+    `sizeId` INTEGER NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `address` JSON NOT NULL,
+    `status` ENUM('active', 'inactive') NOT NULL,
+    `notes` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
